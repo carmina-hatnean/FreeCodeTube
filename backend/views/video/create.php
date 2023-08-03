b10b9ac0c994076a1dd9ba2fa21f36727f86481e
Carmina <carmina.hatnean@leviatan.ro>
2023-08-03 16:26:23 +0300

    freecodetube

--- backend/views/video/create.php
+++ backend/views/video/create.php
@@ -13,19 +13,27 @@ $this->params['breadcrumbs'][] = $this->title;
 
     <h1><?= Html::encode($this->title) ?></h1>
 
-    <div>
+    <div class="d-flex flex-column justify-content-center align-items-center"
 
-        <div class="upload-icon">
-            <i class="fa-regular fa-arrow-up-from-bracket"></i>
-        </div>
-
-        <p>Drag and drop video files to upload</p>
-        <p class="text-muted">Your videos will be private until you publish them.</p>
-
-        <button class="btn btn-primary btn-file">
-            Select File
-            <input type="file" id="videoFile" name="video">
-        </button>
+<div>
+    <div class="upload-icon">
+        <i class="fa-regular fa-arrow-up-from-bracket"></i>
     </div>
+    <br>
+    <p class="m-0">Drag and drop video files to upload</p>
+    <br>
+    <p class="text-muted">Your videos will be private until you publish them.</p>
 
+    <?php \yii\widgets\ActiveForm::begin([
+            'options' => ['enctype' => 'multipart/form-data']
+    ]) ?>
+
+    <button class="btn btn-primary btn-file">
+        Select File
+        <input type="file" id="videoFile" name="video">
+    </button>
+
+    <?php \yii\widgets\ActiveForm::end() ?>
+
+</div>
 </div>
